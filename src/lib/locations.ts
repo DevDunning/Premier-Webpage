cf9909f404747b86a3c24529031bfa611743c221
DevDunning <DevDunning@outlook.com>
2026-06-10 13:13:37 -0500

    Updated # and maps

--- src/lib/locations.ts
+++ src/lib/locations.ts
@@ -1,15 +1,15 @@
 export const locations = {
   mckenzie: {
     name: "McKenzie Showroom",
-    phone: "tel:+17318885555",
+    phone: "tel:+17313527591",
     maps:
-      "https://www.google.com/maps/search/?api=1&query=McKenzie+TN+furniture+store",
+      "https://maps.app.goo.gl/gFD5TbrXmpPdkAKU9",
   },
 
   unionCity: {
     name: "Union City Showroom",
-    phone: "tel:+17318885555",
+    phone: "tel:+17318855777",
     maps:
-      "https://www.google.com/maps/search/?api=1&query=Union+City+TN+furniture+store",
+      "https://maps.app.goo.gl/LwRo3MbsjddsmVTS7",
   },
 };
